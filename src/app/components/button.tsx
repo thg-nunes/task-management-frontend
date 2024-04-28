cf54e71bba4dde99e0284f856ac80516193848ce
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-04-28 13:41:26 -0300

    feat: adiciona possibilidade de exibir um spinner

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,17 +1,24 @@
 import { twMerge } from 'tailwind-merge'
 import { ButtonHTMLAttributes } from 'react'
 
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isLoading?: boolean
+}
+
 /**
  * @function Button - função responsável por retornar um botão com estilos pré-definidos que pode
  * ser reutilizado em qualquer lugar do app, esse componente tem a possibilidade de sobreescrita
  * de estilo sem a possibilidade de conflito de classes
  * @param {ButtonHTMLAttributes<HTMLButtonElement>} ButtonProps - o componente pode receber qualquer
  * propriedade existente na tag button
+ * @param {boolean} [ButtonProps.isLoading=false] - caso o botão seja do tipo submit é possível exibir
+ * um spinner através dessa prop quando ele for clicado
  */
 export const Button = ({
+  isLoading = false,
   children,
   ...rest
-}: ButtonHTMLAttributes<HTMLButtonElement>): JSX.Element => {
+}: ButtonProps): JSX.Element => {
   return (
     <button
       className={twMerge(
@@ -20,7 +27,17 @@ export const Button = ({
       )}
       {...rest}
     >
-      {children}
+      {isLoading ? (
+        <>
+          <span
+            data-testid="spinner"
+            className="size-4 animate-spin rounded-full border-2 border-white border-t-transparent"
+          />
+          {children}
+        </>
+      ) : (
+        children
+      )}
     </button>
   )
 }
