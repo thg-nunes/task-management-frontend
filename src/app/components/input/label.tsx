4537e6b99f623dc5fdbcb395082faebc98592bd0
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-01 14:41:52 -0300

    chore: adiciona docs nos componentes de /input

--- src/app/components/input/label.tsx
+++ src/app/components/input/label.tsx
@@ -5,6 +5,14 @@ type InputLabelProps = LabelHTMLAttributes<HTMLLabelElement> & {
   labelText: string
 }
 
+/**
+ * @function InputLabel - função usada para criar e retornar uma label para usar com um input
+ * @param {InputLabelProps} InputLabel - esse componente recebe todas as props
+ * existentes na tag label
+ * @param {boolean} InputLabel.labelText - usada para atribuir o texto a ser renderizado pela
+ * label
+ * @example uma das formas de usar o componente: <InputLabel htmlFor="emailInput" labelText="email" />
+ */
 export const InputLabel = ({
   labelText,
   className,
--- src/app/components/input/root.tsx
+++ src/app/components/input/root.tsx
@@ -4,7 +4,21 @@ import { twMerge } from 'tailwind-merge'
 type InputContainerProps = HTMLAttributes<HTMLDivElement> & {
   inputErrorMessage?: string
 }
-
+/**
+ * @function InputContainer - função responsável por retornar o componente que pode ser
+ * usado como container para uma label e input juntos
+ * @param {InputContainerProps} InputContainerProps - o componente recebe todas as props
+ * existentes na tag div
+ * @param {string} InputContainerProps.inputErrorMessage - usada para passar a mensagem de
+ * erro gerada por um determinado input
+ * @example uma das formas de usar o componente: <InputContainer
+                                                    className="items-end gap-2"
+                                                    inputErrorMessage={email?.message}
+                                                  >
+                                                    <InputLabel htmlFor="emailInput" labelText="email" />
+                                                    <InputElement id="emailInput" placeholder="Email" type="email" hasError={!!email?.message} />
+                                                  </InputContainer>
+ */
 export const InputContainer = ({
   children,
   className,
