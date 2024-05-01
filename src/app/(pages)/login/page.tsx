a6d6ff75efb940de0dd00770e33d90ae0bbb2f4e
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-01 16:11:06 -0300

    feat: inicia construção da página de login

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from 'next'
+import { FcSerialTasks } from 'react-icons/fc'
+import { SignInForm } from './client-components/signForm'
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description:
+    'Essa página é responsável por permitir que o usuário realize seu\
+  login no sistema ou seja redirecionado para a página onde possa criar sua conta.',
+}
+
+export default function Login() {
+  return (
+    <main className="h-svh px-6">
+      <div className="grid h-full grid-rows-4">
+        <div className="mx-auto flex w-max items-center gap-3">
+          <FcSerialTasks className="size-8" />
+          <p>TaskMgm</p>
+        </div>
+
+        <div className="row-span-3 mx-auto h-max w-full space-y-6">
+          <h2 className="mx-auto text-center text-2xl font-semibold">
+            Bem Vindo de volta!
+          </h2>
+          <SignInForm />
+        </div>
+      </div>
+    </main>
+  )
+}
