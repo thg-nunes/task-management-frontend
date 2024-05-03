7885a75bcb66a4c153a29fcd0e72e5e60ae56cf8
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-03 15:03:26 -0300

    feat: adiciona possibilidade de estilizar o container da logo

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -1,22 +1,32 @@
 import { twMerge } from 'tailwind-merge'
 import { FcSerialTasks } from 'react-icons/fc'
+import { HTMLAttributes } from 'react'
 
-type LogoProps = {
+type LogoProps = HTMLAttributes<HTMLDivElement> & {
   textStyle?: string
   iconStyle?: string
 }
 
 /**
  * @function Logo - função usada para conter a composição da logo do app
- * @param {Object} LogoProps
+ * @param {LogoProps} LogoProps - o componente reber todas as props default da tag div
+ * mais 2 para estilizar o texto e imagem da logo
  * @param {string} [LogoProps.textStyle=''] - caso necessário mudar estilo do texto
  * em algum lugar, basta passar as classes tailwind para essa prop
  * @param {string} [LogoProps.iconStyle=''] - caso necessário mudar estilo do icone
  * em algum lugar, basta passar as classes tailwind para essa prop
  */
-export const Logo = ({ textStyle = '', iconStyle = '' }: LogoProps): JSX.Element => {
+export const Logo = ({
+  textStyle = '',
+  iconStyle = '',
+  className,
+  ...rest
+}: LogoProps): JSX.Element => {
   return (
-    <div className="mx-auto flex w-max items-center gap-3">
+    <div
+      className={twMerge('mx-auto flex w-max items-center gap-3', className)}
+      {...rest}
+    >
       <FcSerialTasks className={twMerge('size-12', iconStyle)} />
       <p className={twMerge('text-xl', textStyle)}>TaskMgm</p>
     </div>
