89d41d31c9aef8869b2498d4c2eebf731fc0a585
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-03 11:45:56 -0300

    feat: adiciona inclusão de credenciais enviadas pelo server

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -6,4 +6,5 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const apolloClient = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_APOLLO_SERVER_URI,
   cache: new InMemoryCache(),
+  credentials: 'include',
 })
