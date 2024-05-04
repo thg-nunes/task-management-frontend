ba62f136457cb16ade2bae2668f4a53ffb93a43b
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-04 12:13:04 -0300

    test: garante que os campos do formulario de signin sejam requeridos para enviar o formulário

--- __tests__/hooks/pages/login.spec.tsx
+++ __tests__/hooks/pages/login.spec.tsx
@@ -1,10 +1,10 @@
 import { useRouter } from 'next/navigation'
 import { MockedResponse } from '@apollo/client/testing'
-import { act, renderHook } from '@testing-library/react'
+import { act, renderHook, waitFor } from '@testing-library/react'
 
 import { toastify } from '@utils/toastify'
 import { GQL_SIGNIN } from '@gql/mutations/user'
-import { useHandleSignIn } from '@hooks/pages/login'
+import { useConfigSignForm, useHandleSignIn } from '@hooks/pages/login'
 import { ApolloClientTestProvider } from '@utils/apollo-client-test-provider'
 
 jest.mock('@utils/toastify')
@@ -67,5 +67,21 @@ describe('hook useHandleSignIn', () => {
     expect(toastify).toHaveBeenCalledWith(fakeError.message, {
       type: 'error',
     })
+  })
+
+  it('ensures that email and password fields is required', async () => {
+    const { result } = renderHook(() => useConfigSignForm())
+
+    act(() => {
+      result.current.handleSubmit({
+        email: '',
+        password: '',
+      } as any)
+    })
+
+    const { errors } = await result.current.control._executeSchema(['email', 'password'])
+
+    expect(errors.email?.type).toBe('required')
+    expect(errors.password?.type).toBe('required')
   })
 })
