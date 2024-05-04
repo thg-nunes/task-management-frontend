2d5a6ec6b12cd9346e928f817e36f8f303675c61
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-04 10:52:47 -0300

    test: garante que um toast é exibido ao user se o sign falhar

--- __tests__/hooks/pages/login.spec.tsx
+++ __tests__/hooks/pages/login.spec.tsx
@@ -1,10 +1,13 @@
+import { useRouter } from 'next/navigation'
+import { MockedResponse } from '@apollo/client/testing'
 import { act, renderHook } from '@testing-library/react'
 
+import { toastify } from '@utils/toastify'
+import { GQL_SIGNIN } from '@gql/mutations/user'
 import { useHandleSignIn } from '@hooks/pages/login'
 import { ApolloClientTestProvider } from '@utils/apollo-client-test-provider'
-import { MockedResponse } from '@apollo/client/testing'
-import { GQL_SIGNIN } from '@gql/mutations/user'
-import { useRouter } from 'next/navigation'
+
+jest.mock('@utils/toastify')
 
 jest.mock('next/navigation')
 const useRouterMock = useRouter as jest.Mock
@@ -39,5 +42,30 @@ describe('hook useHandleSignIn', () => {
     await act(() => result.current.signInMutationFn())
 
     expect(useRouterMock().push).toHaveBeenCalledWith('/home')
+  })
+
+  it('should call toastify if sign fails', async () => {
+    const fakeError = new Error('sign fail')
+    const fakeQueryError: MockedResponse = {
+      request: {
+        query: GQL_SIGNIN,
+        operationName: 'sign',
+      },
+      error: fakeError,
+    }
+
+    const { result } = renderHook(() => useHandleSignIn(), {
+      wrapper: ({ children }) => (
+        <ApolloClientTestProvider mocks={[fakeQueryError]}>
+          {children}
+        </ApolloClientTestProvider>
+      ),
+    })
+
+    await act(() => result.current.signInMutationFn())
+
+    expect(toastify).toHaveBeenCalledWith(fakeError.message, {
+      type: 'error',
+    })
   })
 })
