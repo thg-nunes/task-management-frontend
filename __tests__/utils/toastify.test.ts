c75a9de3f04c468faebbc7cf93b072da349977bb
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-04 10:07:31 -0300

    test: garante que o tipo do toast possa ser selecionado ao invocar a função

--- __tests__/utils/toastify.test.ts
+++ __tests__/utils/toastify.test.ts
@@ -5,8 +5,26 @@ import { toastify } from '@utils/toastify'
 
 describe('@util/toastify', () => {
   it('should render toast with provided message', () => {
-    renderHook(() => toastify('any message'))
+    renderHook(() =>
+      toastify('any message', {
+        type: 'success',
+      })
+    )
 
-    expect(toastify).toHaveBeenCalledWith('any message')
+    expect(toastify).toHaveBeenCalledWith('any message', {
+      type: 'success',
+    })
+  })
+
+  it('should render toast with provided type', () => {
+    renderHook(() =>
+      toastify('any message', {
+        type: 'error',
+      })
+    )
+
+    expect(toastify).toHaveBeenCalledWith('any message', {
+      type: 'error',
+    })
   })
 })
