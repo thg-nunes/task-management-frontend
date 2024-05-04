0cad812a52757d1866a27165b24daabef1fbdff2
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-04 19:26:00 -0300

    test: garante que a key taskMgm@islogged é removida do localStorage e o usuário é redirecionado para pagina de login

---
+++ __tests__/hooks/signout.spec.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useRouter } from 'next/navigation'
+import { MockedResponse } from '@apollo/client/testing'
+import { act, renderHook, waitFor } from '@testing-library/react'
+
+import { useSignOut } from '@hooks/signOut'
+import { GQL_SIGNOUT } from '@gql/mutations/user'
+import { ApolloClientTestProvider } from '@utils/apollo-client-test-provider'
+
+jest.mock('next/navigation')
+const useRouterMock = useRouter as jest.Mock
+
+describe('hook signOut', () => {
+  it('should remove item from localStorage and redirect user to login page on call useSignOut hook', async () => {
+    const fakePush = jest.fn()
+    useRouterMock.mockReturnValue({ push: fakePush })
+
+    const fakeSignOutResponse: MockedResponse = {
+      request: {
+        query: GQL_SIGNOUT,
+        operationName: 'signOut',
+      },
+      result: {
+        data: {
+          signOut: true,
+        },
+      },
+    }
+
+    const removeItemSpy = jest.spyOn(Storage.prototype, 'removeItem')
+
+    const { result } = renderHook(() => useSignOut(), {
+      wrapper: ({ children }) => (
+        <ApolloClientTestProvider mocks={[fakeSignOutResponse]}>
+          {children}
+        </ApolloClientTestProvider>
+      ),
+    })
+
+    await act(async () => await result.current.handlSignout())
+
+    expect(removeItemSpy).toHaveBeenCalledWith('taskMgm@islogged')
+    await waitFor(() => expect(fakePush).toHaveBeenCalledWith('/login'))
+  })
+})
