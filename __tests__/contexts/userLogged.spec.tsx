f734166f1f44d5c4dbf955b70521383f23698e29
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-04 15:16:34 -0300

    test: garante que o localStorage.getItem é chamado sempre que o pathname mudar

--- __tests__/contexts/userLogged.spec.tsx
+++ __tests__/contexts/userLogged.spec.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { usePathname } from 'next/navigation'
 import { render } from '@testing-library/react'
 
 import { UserIsLoggedProvider } from '@context/userIsLogged'
@@ -10,6 +11,7 @@ jest.mock('react', () => ({
 jest.mock('next/navigation', () => ({
   usePathname: jest.fn(),
 }))
+const usePathnameMock = usePathname as jest.Mock
 
 describe('<UserIsLogged />', () => {
   it('should call getItem method of localStorage when UserIsLoggedProvider render', async () => {
@@ -22,5 +24,27 @@ describe('<UserIsLogged />', () => {
     )
 
     expect(getItemSpy).toHaveBeenCalledWith('taskMgm@islogged')
+  })
+
+  it('should call getItem method of localStorage when pathname change', async () => {
+    usePathnameMock.mockReturnValue('/')
+
+    const getItemSpy = jest.spyOn(Storage.prototype, 'getItem')
+
+    const { rerender } = render(
+      <UserIsLoggedProvider>
+        <div>App</div>
+      </UserIsLoggedProvider>
+    )
+
+    usePathnameMock.mockReturnValue('/any_path')
+
+    rerender(
+      <UserIsLoggedProvider>
+        <div>App</div>
+      </UserIsLoggedProvider>
+    )
+
+    expect(getItemSpy.mock.calls.length).toBe(2)
   })
 })
