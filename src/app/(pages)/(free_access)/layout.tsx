3d0f519c2f8d9943fc44effa5b375177d3a9da8c
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-05-28 13:25:00 -0300

    refactor: usando hook correto para ação no client

--- src/app/(pages)/(free_access)/layout.tsx
+++ src/app/(pages)/(free_access)/layout.tsx
@@ -1,15 +1,16 @@
 'use client'
 import React, { ReactNode } from 'react'
-import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 
 export default function FreeAccessLayout({ children }: { children: ReactNode }) {
+  const router = useRouter()
   const session = useSession()
 
   React.useEffect(() => {
-    if (session.status === 'authenticated') redirect('/home')
+    if (session.status === 'authenticated') router.push('/home')
     return () => {}
-  }, [session.status])
+  }, [session.status, router])
 
   return <>{session.status === 'unauthenticated' && children}</>
 }
