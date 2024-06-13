2af9bd1cd3f4e7e0348257a151c5d7a20603be8e
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-06-13 12:44:14 -0300

    refactor: adiciona possibilidade de definir estilo do UserPhoto caso tenha ou nao um avatar pra exibir

--- src/app/components/userPhoto.tsx
+++ src/app/components/userPhoto.tsx
@@ -1,8 +1,21 @@
 import Image from 'next/image'
+import { FaUserAlt } from 'react-icons/fa'
 import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 
 type UserPhotoProps = {
   src: string | StaticImport
+}
+
+/**
+ * @namespace USER_PHOTO_CONTAINER_STYLE - esse objeto contém duas keys com os possíveis estilos
+ * para a dive que serve de container da imagem do usuário
+ * @property {string} USER_PHOTO_CONTAINER_STYLE.default - estilo caso o usuário tenha um avatar
+ * @property {string} USER_PHOTO_CONTAINER_STYLE.empty - estilo caso o usuário não tenha um avatar
+ */
+const USER_PHOTO_CONTAINER_STYLE = {
+  default: 'w-max overflow-hidden rounded-full border-2 border-white md:size-44',
+  empty:
+    'flex w-max items-center justify-center bg-zinc-500 overflow-hidden rounded-full border-2 border-white md:size-44',
 }
 
 /**
@@ -11,14 +24,21 @@ type UserPhotoProps = {
  */
 export const UserPhoto = ({ src }: UserPhotoProps): JSX.Element => {
   return (
-    <div className="w-max overflow-hidden rounded-full border-2 border-white">
-      <Image
-        src={src}
-        width={176}
-        height={176}
-        className="md:size-44"
-        alt="foto do usuário ou imagem padrão caso nao tenha uma foto"
-      />
+    <div
+      className={
+        src ? USER_PHOTO_CONTAINER_STYLE.default : USER_PHOTO_CONTAINER_STYLE.empty
+      }
+    >
+      {src ? (
+        <Image
+          src={src}
+          width={176}
+          height={176}
+          alt="foto do usuário ou imagem padrão caso nao tenha uma foto"
+        />
+      ) : (
+        <FaUserAlt size={40} className="md:size-10" />
+      )}
     </div>
   )
 }
